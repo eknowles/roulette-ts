ffe9b6e3e480e35d3c256877552daee91a7e093a
Edward Knowles <mail@eknowles.com>
2018-03-15 16:03:55 +0000

    test(game): add missing tests for game index exports

---
+++ src/game/__tests__/index.spec.ts
@@ -0,0 +1,10 @@
+import * as module from '../index';
+
+describe('Game', () => {
+  it('should export models', () => {
+    expect(module.Game).toBeDefined();
+    expect(module.Player).toBeDefined();
+    expect(module.Round).toBeDefined();
+    expect(module.Table).toBeDefined();
+  });
+});
