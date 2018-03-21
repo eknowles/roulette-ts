aebae85b452b3e9eb0a2897fe4c0d1cda9ae421f
Edward Knowles <mail@eknowles.com>
2018-03-21 10:44:25 +0000

    feat(game.statistics): add Statistics class for previous winners

---
+++ src/game/models/__tests__/statistics.spec.ts
@@ -0,0 +1,20 @@
+import { POSITIONS } from '../../constants/positions';
+import { Statistics } from '../statistics';
+
+describe('Class', () => {
+  describe('Statistics', () => {
+    describe('getPositionCount()', () => {
+      it('should return an object of all positionIds', () => {
+        const result = Statistics.getPositionCount([]);
+        POSITIONS.forEach((pos) => {
+          expect(result[pos.id]).toBeDefined();
+          expect(typeof result[pos.id]).toBe('number');
+        });
+      });
+      it('should return just the positions with typeId when specified in the second param', () => {
+        expect(Statistics.getPositionCount([], 'ROW')).toEqual({P_ROW: 0});
+        expect(Statistics.getPositionCount([], 'ZERO')).toEqual({P_ZERO: 0});
+      });
+    });
+  });
+});
---
+++ src/game/models/statistics.ts
@@ -0,0 +1,32 @@
+import { IPosition, POSITIONS } from '../constants/positions';
+
+export interface IPositionStatistic {
+  [positionId: string]: number; // count
+}
+
+/**
+ * This class contains methods for summarising the previous round numbers and the positions that were winners
+ */
+export class Statistics {
+
+  /**
+   * Calculate the overall position statistics of winning numbers
+   * @param {number[]} winningNumbers
+   * @param {string} typeId
+   * @return {IPositionStatistic}
+   */
+  public static getPositionCount(winningNumbers: number[], typeId?: string): IPositionStatistic {
+    let positions = POSITIONS;
+
+    if (typeId) {
+      positions = positions.filter((pos) => pos.typeId === typeId);
+    }
+
+    return positions.reduce((stats: IPositionStatistic, current: IPosition) => {
+      stats[current.id] = winningNumbers.reduce((total: number, number: number) => {
+        return current.winners.includes(number) ? total + 1 : total;
+      }, 0);
+      return stats;
+    }, {});
+  }
+}
