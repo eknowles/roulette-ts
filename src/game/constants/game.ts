7087ef37cd2f9a36bc620799fa68a7ff34ddd157
Edward Knowles <mail@eknowles.com>
2018-03-21 15:12:04 +0000

    chore(game.constants): add game rules as constants

---
+++ src/game/constants/game.ts
@@ -0,0 +1,29 @@
+/**
+ * The maximum value a player can deposit in one go
+ * @type {number}
+ */
+export const MAX_DEPOSIT_VALUE = 10000;
+
+/**
+ * The minimum value a player can deposit
+ * @type {number}
+ */
+export const MIN_DEPOSIT_VALUE = 1;
+
+/**
+ * The maximum total value of all positions that can be placed in a round
+ * @type {number}
+ */
+export const MAX_OUTSIDE_BET_VALUE = 500;
+
+/**
+ * The maximum total value of all positions that can be placed in a round
+ * @type {number}
+ */
+export const MIN_OUTSIDE_BET_VALUE = 5;
+
+/**
+ * The maximum total value of all positions that can be placed in a round
+ * @type {number}
+ */
+export const MAX_INSIDE_BET_VALUE = 5000;
